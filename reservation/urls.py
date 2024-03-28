9b66e4202160a4177b825b014abc78de5e6deb5f
ubachicodes . <prettyestmail@gmail.com>
2024-03-28 17:51:23 +0000

    CSS updated, static files moved

--- reservation/urls.py
+++ reservation/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from . import views
 
-
-app_name = 'reservation'
-
 urlpatterns = [
     path('reservation/', views.reservation, name='reservation'),
+    # Other URL patterns for your application...
 ]
