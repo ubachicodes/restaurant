22be2ae054c4877692c9b412e4e5b5552532ecf5
ubachicodes . <prettyestmail@gmail.com>
2024-03-29 12:17:14 +0000

    About us edited, moved static and templates to root dir, site loading now

--- about/urls.py
+++ about/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-    path('', views.about, name='about'),
-    # Add more paths as needed for additional about page views
+    path('about/', views.about, name='about'),
 ]
--- about/views.py
+++ about/views.py
@@ -1,6 +1,6 @@
+# views.py
+
 from django.shortcuts import render
 
-# Create your views here.
 def about(request):
-    # Render contact page
     return render(request, 'about/about.html')
