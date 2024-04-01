0532eb0eddc7279127e3d33f5c4227685e13e10b
ubachicodes . <prettyestmail@gmail.com>
2024-04-01 18:40:49 +0000

    Contact page url added to main app

--- contact/urls.py
+++ contact/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from . import views
+from . import views  # Import the views module
 
 urlpatterns = [
-    path('contact/', views.contact_form, name='contact_form'),
+    path('contact/', views.contact_form, name='contact_form'),  # Use the view function from the imported views module
     path('success/', views.success, name='success'),
 ]
+
--- nourish/urls.py
+++ nourish/urls.py
@@ -4,13 +4,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
-
 urlpatterns = [
-    path('admin/', admin.site.urls), # URL for admins
-    path('', include('home.urls')),  # URL for the home page
+    path('admin/', admin.site.urls),  # URL for admins
+    path('', include('home.urls')),    # URL for the home page
     path('about/', include('about.urls')),  # URL for the about page
     path('meals/', include('meals.urls')),  # URL for the menu page
     path('reservation/', include('reservation.urls')),  # URL for the reservation page
+    path('contact/', include('contact.urls')),  # URL for the contact page
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
